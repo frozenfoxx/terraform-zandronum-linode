9a55cfc2efcbd86cefbb5df6d215d46026900fae
FrozenFOXX <frozenfoxx@churchoffoxx.net>
2020-06-04 23:58:08 -0700

    Added main.tf

---
+++ main.tf
@@ -0,0 +1,33 @@
+resource "linode_instance" "main" {
+  label           = var.name
+  image           = var.image
+  region          = var.region
+  type            = var.type
+  authorized_keys = var.authorized_keys
+  root_pass       = var.root_pass
+
+  group = var.group
+  tags = var.tags
+
+  connection {
+    type        = "ssh"
+    user        = "root"
+    agent       = "false"
+    password    = var.root_pass
+    private_key = var.private_key
+    host        = self.ip_address
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/scripts"
+    destination = "/tmp"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod 755 /tmp/scripts/*.sh",
+      "/tmp/scripts/install_docker.sh",
+      "CONFIG=${var.config} /tmp/scripts/install_zandronum.sh"
+    ]
+  }
+}
